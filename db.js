69e7756b25b84e6e1c4ab9e5b50b1e0a89c3a95d
Maobugichi <immansamuel846@gmail.com>
2025-09-23 21:50:59 +0100

    chore:test db.js3

--- db.js
+++ db.js
@@ -5,6 +5,17 @@ dotenv.config();
 
 const { Pool } = pkg;
 
+console.log("CONNECTION_STRING from env:", process.env.CONNECTION_STRING);
+console.log("All env vars:", Object.keys(process.env));
+
+const connectionString = process.env.CONNECTION_STRING;
+
+if (!connectionString) {
+    console.error("❌ CONNECTION_STRING environment variable is not set!");
+    process.exit(1);
+}
+
+
 const dbUrl = new URL(process.env.CONNECTION_STRING)
 
 const pool = new Pool({
