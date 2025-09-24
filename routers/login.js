65d1c846d88e71278626f3a210f61f793bceb7fe
Maobugichi <immansamuel846@gmail.com>
2025-09-24 10:44:48 +0100

    chore:check cookie

--- routers/login.js
+++ routers/login.js
@@ -14,10 +14,11 @@ loginRouter.post("/",async (req,res) => {
     try {
         const { email , password } = req.body;
         const result = await  pool.query('SELECT * FROM users WHERE email = $1',[email]);
-         console.log(email)
+        
         if (result.rows.length === 0) {
             return res.status(401).json({ message: "user doesnt exist" });
         }
+        console.log(result)
         const user = result.rows[0]
         const dbPassword = user.password
         const userId = user.id
@@ -27,11 +28,14 @@ loginRouter.post("/",async (req,res) => {
         }
 
         const token = jwt.sign({id: userId, username: user.username, email: user.email},jwtSecret,{ expiresIn:'1d'});
+
+        console.log(token)
         res.cookie("token",token,{
-            httpOnly:true,
-            sameSite:"lax",
-            secure:process.env.NODE_ENV === "production",
-            maxAge: 24 * 60 * 60 * 1000
+            httpOnly: true,
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 24 * 60 * 60 * 1000,
+            path: "/"  
         })
         res.status(200).json({username:user.username, userId , email , onboarded:user.onboarded})
 
